f8c25649b049cf83c083a2150b29ec5d83ee1556
Eagles27 <92862523+Eagles27@users.noreply.github.com>
2021-11-30 17:54:06 +0100

    a

--- QuoridorCB/setup.c
+++ QuoridorCB/setup.c
@@ -9,10 +9,15 @@ void setup(int *nombrejouer, int debut, int mat[17][17])
         printf("\nJoueur %d:\nPlacer votre Pion sur la premiere ligne\n",debut);
         if (debut == 1)
         {
-            printf("Choisir une ligne A --> I\n");
-            fflush(stdin);
-            scanf("%c",&cJ1);
-            EcJ1 = traduction(cJ1);
+            do
+            {
+                printf("Choisir une ligne A --> Q\n");
+                fflush(stdin);
+                scanf("%c",&cJ1);
+            }
+            while ((cJ1!='A')&&(cJ1!='C')&&(cJ1!='E')&&(cJ1!='G')&&(cJ1!='I')&&(cJ1!='K')&&(cJ1!='M')&&(cJ1!='O')&&(cJ1!='Q'));
+
+            EcJ1 = traduction(cJ1);;
             mat[0][EcJ1] = 5;
             affiche(mat);
 
@@ -21,9 +26,14 @@ void setup(int *nombrejouer, int debut, int mat[17][17])
             affiche(mat);
 
             printf("\nJoueur 2:\nPlacer votre Pion sur la premiere ligne\n");
-            printf("Choisir une ligne A --> I\n");
-            fflush(stdin);
-            scanf("%c",&cJ2);
+
+            do
+            {
+                printf("Choisir une ligne A --> Q\n");
+                fflush(stdin);
+                scanf("%c",&cJ2);
+            }
+            while ((cJ2!='A')&&(cJ2!='C')&&(cJ2!='E')&&(cJ2!='G')&&(cJ2!='I')&&(cJ2!='K')&&(cJ2!='M')&&(cJ2!='O')&&(cJ2!='Q'));
             EcJ2 = traduction(cJ2);
             mat[16][EcJ2] = 7;
             system("cls");
@@ -33,22 +43,29 @@ void setup(int *nombrejouer, int debut, int mat[17][17])
 
         else
         {
+            do
+            {
+                printf("Choisir une ligne A --> Q\n");
+                fflush(stdin);
+                scanf("%c",&cJ2);
+            }
+            while ((cJ2!='A')&&(cJ2!='C')&&(cJ2!='E')&&(cJ2!='G')&&(cJ2!='I')&&(cJ2!='K')&&(cJ2!='M')&&(cJ2!='O')&&(cJ2!='Q'));
 
-            printf("Choisir une ligne A --> I\n");
-            fflush(stdin);
-            scanf("%c",&cJ2);
             EcJ2 = traduction(cJ2);
             mat[16][EcJ2] = 7;
             affiche(mat);
 
-
             system("cls");
             affiche(mat);
-
             printf("\nJoueur 1:\nPlacer votre Pion sur la premiere ligne\n");
-            printf("Choisir une ligne A --> I\n");
-            fflush(stdin);
-            scanf("%c",&cJ1);
+
+            do
+            {
+                printf("Choisir une ligne A --> Q\n");
+                fflush(stdin);
+                scanf("%c",&cJ1);
+            }while ((cJ1!='A')&&(cJ1!='C')&&(cJ1!='E')&&(cJ1!='G')&&(cJ1!='I')&&(cJ1!='K')&&(cJ1!='M')&&(cJ1!='O')&&(cJ1!='Q'));
+
             EcJ1 = traduction(cJ1);
             mat[0][EcJ1] = 5;
             system("cls");
