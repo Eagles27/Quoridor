7ddec4f38a151fa937e54ef8aa5b2901a7d55049
Pierre <70819571+swiipius@users.noreply.github.com>
2021-12-01 13:24:47 +0100

    c'est repare

--- QuoridorCB/Header_Pierre.h
+++ QuoridorCB/Header_Pierre.h
@@ -5,6 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct joueur{
+
+    char nomJ[50];
+    int scoreP;
+    char jetonJ;
+    int barrieresR;
+    int numeroJ;
+
+}t_joueur;
+
 void deplacement(int dep[3], int matrice[17][17], int nombrejoueur);
 void genematrice(int tab[17][17]);
 void affiche(int tab[17][17]);
