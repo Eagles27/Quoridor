13c32076cf930cc29366f9261a951389f3b97015
Eagles27 <92862523+Eagles27@users.noreply.github.com>
2021-12-02 19:55:57 +0100

    Modification du setup
    passage du tour pour le setup pour 2 joueurs
    
    4 joueurs tjrs pas implémenté

--- QuoridorCB/setup.c
+++ QuoridorCB/setup.c
@@ -4,14 +4,23 @@ void setup(int *nombrejouer, int debut, int mat[17][17],t_joueur player[4], int 
 {
     char cJ1,cJ2,cJ3,cJ4;
     int EcJ1,EcJ2,EcJ3,EcJ4;
+
+    ///Reset Point de la partie
+
+    for(int i=0;i<4;i++)
+    {
+        player[i].scoreP = 0;
+    }
+
+
     if(*nombrejouer == 2)
     {
-        printf("\nJoueur %d:\nPlacer votre Pion sur la premiere ligne\n",debut);
+
         if (debut == 1)
-        {
+        { tour = 1;
             do
-            {
-                printf("Choisir une ligne A --> Q\n");
+            {   printf("%s:\n\n",player[0].nomJ);
+                printf("Choisissez une ligne A --> Q\n");
                 fflush(stdin);
                 scanf("%c",&cJ1);
             }
@@ -19,35 +28,43 @@ void setup(int *nombrejouer, int debut, int mat[17][17],t_joueur player[4], int 
 
             EcJ1 = traduction(cJ1);;
             mat[0][EcJ1] = 5;
-            matrice_propre(mat);
+
+            tour = 2;
 
 
             system("cls");
             matrice_propre(mat);
             menu_cote(tour,nombrejouer,player);
 
-            printf("\nJoueur 2:\nPlacer votre Pion sur la premiere ligne\n");
+
+
+
 
             do
             {
-                printf("Choisir une ligne A --> Q\n");
+                printf("%s:\n\n",player[1].nomJ);
+                printf("Choisissez une ligne A --> Q\n");
                 fflush(stdin);
                 scanf("%c",&cJ2);
             }
             while ((cJ2!='A')&&(cJ2!='C')&&(cJ2!='E')&&(cJ2!='G')&&(cJ2!='I')&&(cJ2!='K')&&(cJ2!='M')&&(cJ2!='O')&&(cJ2!='Q'));
             EcJ2 = traduction(cJ2);
             mat[16][EcJ2] = 7;
+
             system("cls");
             matrice_propre(mat);
             menu_cote(tour,nombrejouer,player);
 
         }
 
+
         else
         {
+            tour = 2;
             do
             {
-                printf("Choisir une ligne A --> Q\n");
+                printf("%s:\n\n",player[1].nomJ);
+                printf("Choisissez une ligne A --> Q\n");
                 fflush(stdin);
                 scanf("%c",&cJ2);
             }
@@ -55,26 +72,29 @@ void setup(int *nombrejouer, int debut, int mat[17][17],t_joueur player[4], int 
 
             EcJ2 = traduction(cJ2);
             mat[16][EcJ2] = 7;
-            matrice_propre(mat);
+
 
             system("cls");
             matrice_propre(mat);
             menu_cote(tour,nombrejouer,player);
-            printf("\nJoueur 1:\nPlacer votre Pion sur la premiere ligne\n");
+
 
             do
             {
-                printf("Choisir une ligne A --> Q\n");
+                printf("%s:\n\n",player[0].nomJ);
+
+                printf("Choisissez une ligne A --> Q\n");
                 fflush(stdin);
                 scanf("%c",&cJ1);
             }
             while ((cJ1!='A')&&(cJ1!='C')&&(cJ1!='E')&&(cJ1!='G')&&(cJ1!='I')&&(cJ1!='K')&&(cJ1!='M')&&(cJ1!='O')&&(cJ1!='Q'));
 
+            tour = 1;
             EcJ1 = traduction(cJ1);
             mat[0][EcJ1] = 5;
             system("cls");
-            menu_cote(tour,nombrejouer,player);
             matrice_propre(mat);
+            menu_cote(tour,nombrejouer,player);
 
 
         }
@@ -84,5 +104,6 @@ void setup(int *nombrejouer, int debut, int mat[17][17],t_joueur player[4], int 
     else
     {
         printf("Non dispo\n");
+        system("PAUSE");
     }
 }
