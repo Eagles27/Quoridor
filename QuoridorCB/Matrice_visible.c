df40fbb429160a4b25e8b2830b2a8f100185d1cf
Agrafes <92798770+Agrafes@users.noreply.github.com>
2021-12-12 00:11:26 +0100

    Début des comm adrien

--- QuoridorCB/Matrice_visible.c
+++ QuoridorCB/Matrice_visible.c
@@ -11,9 +11,9 @@
 - rond : 0x0F
 */
 
-void matrice_propre(int tab[17][17], t_joueur player[4], t_IA ia)
+void matrice_propre(int tab[17][17], t_joueur player[4], t_IA ia)//Comm OK
 {
-    ///0.0. DDV temporaire
+    ///0.0.pré-DDV
     char caractereJ[4];
 
     /// Attribution des tokens pour chaques joueurs
@@ -26,10 +26,14 @@ void matrice_propre(int tab[17][17], t_joueur player[4], t_IA ia)
     ///0. DDV
     int i,j;
 
+
+    ///1. Affichage des lettres autour du graphique (horizontal)
     printf("    ");
     for (i = 0 ; i<17 ; i++) printf(" %c", 'A'+i);
     printf("\n\n");
 
+
+    ///2. Affichage des lettres verticales
     for(i=0; i<17; i++)
     {
         printf("%c   ",'a'+i);
@@ -37,13 +41,13 @@ void matrice_propre(int tab[17][17], t_joueur player[4], t_IA ia)
         {
             if(i%2==1)
             {
-                if(j%2==1)
+                if(j%2==1)//Les doubles if permet de se placer exactement sur les croisements
                 {
                     if(tab[i][j]==3){
-                        printf("%c", 0xCE);
+                        printf("%c", 0xCE);//Mur croisement double
                     }
                     else{
-                        printf("%c", 0xC5);
+                        printf("%c", 0xC5);//Mur croisement simple
                     }
                 }
 
@@ -51,45 +55,47 @@ void matrice_propre(int tab[17][17], t_joueur player[4], t_IA ia)
                 {
                     if(tab[i][j]==1){
                         //printf("j%2==0");
-                        printf("%c%c%c", 0xC4, 0xC4, 0xC4);
+                        printf("%c%c%c", 0xC4, 0xC4, 0xC4);//Mur horizontal
                     }
                     else{
-                        printf("%c%c%c", 0xCD, 0xCD, 0xCD);
+                        printf("%c%c%c", 0xCD, 0xCD, 0xCD);//Mur horizontal double
                     }
                 }
             }
             else
             {
+                ///Etapes de traduction
                 if(tab[i][j]==0)
                 {
-                    printf("   ");
+                    printf("   ");//Rien dans la case
                 }
+                //Affichage du jeton du joueur à la bonne place
                 else if(tab[i][j]==5){
-                    printf(" %c ", caractereJ[0]);
+                    printf(" %c ", caractereJ[0]);//J1
                 }
                 else if(tab[i][j]==7){
-                    printf(" %c ", caractereJ[1]);
+                    printf(" %c ", caractereJ[1]);//J2
                 }
                 else if(tab[i][j]==8){
-                    printf(" %c ", caractereJ[2]);
+                    printf(" %c ", caractereJ[2]);//J3
                 }
                 else if(tab[i][j]==9){
-                    printf(" %c ", caractereJ[3]);
+                    printf(" %c ", caractereJ[3]);//J4
                 }
                 else if(tab[i][j]==1){
-                    printf("%c", 0xB3);
+                    printf("%c", 0xB3);//Mur vertical
                 }
                 else if(tab[i][j]==3){
-                    printf("%c", 0xBA);
+                    printf("%c", 0xBA);//Mur vertical double
                 }
                 else if(tab[i][j]==4){
-                    printf(" %c ", 0x01);
+                    printf(" %c ", 0x01);//Case bonus
                 }
                 else if(tab[i][j]==6){
-                    printf(" %c ", ia.jeton);
+                    printf(" %c ", ia.jeton);//Jeton du joueur IA
                 }
             }
         }
-        printf("\n");
+        printf("\n");//Va disparaitre soooooon <3
     }
 }
