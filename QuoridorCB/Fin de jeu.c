1a8558549894473f6643c1b0c7171bc12a0fe0c6
Pierre <70819571+swiipius@users.noreply.github.com>
2021-12-12 02:28:09 +0100

    Commentaire gagner et distribution barriere

--- QuoridorCB/Fin de jeu.c
+++ QuoridorCB/Fin de jeu.c
@@ -1,13 +1,17 @@
 #include "Header_Pierre.h"
 
+///Verifie si un joueur a gagner ou pas
 int gagner(t_joueur player[4], int* Pnombrejoueur, int tour)
 {
+    ///1. Partie a 2J
     if(*Pnombrejoueur == 2)
     {
+        ///1.1. Un joueur n'a plus de barriere
         if(player[tour].barrieresR == 0)
         {
             return 2;
         }
+        ///1.2. Un joueur est arrive au bout du plateau
         else if((player[tour].coordonneY_org == 0)&&(player[tour].coordonneY == 16))
         {
             return 1;
@@ -21,24 +25,30 @@ int gagner(t_joueur player[4], int* Pnombrejoueur, int tour)
             return -1;
         }
     }
+    ///2. Partie a 4j
     else
     {
+        ///2.1.1. Le joueur 1 est arrive au bout du plateau
         if(player[0].coordonneY == 16)
         {
             return 1;
         }
+        ///2.1.2. Le joueur 2 est arrive au bout du plateau
         else if(player[1].coordonneX == 0)
         {
             return 1;
         }
+        ///2.1.3. Le joueur 3 est arrive au bout du plateau
         else if(player[2].coordonneY == 0)
         {
             return 1;
         }
+        ///2.1.4. Le joueur 4 est arrive au bout du plateau
         else if(player[3].coordonneX == 16)
         {
             return 1;
         }
+        ///2.2. Un jour n'a plus de barriere
         else if(player[tour].barrieresR == 0)
         {
             return 2;
--- QuoridorCB/distribution_barriere.c
+++ QuoridorCB/distribution_barriere.c
@@ -1,17 +1,21 @@
 #include "Header_Pierre.h"
 
+///Suivant le nombre de joueur, donne le nombre adequat de barriere a chacun
 void distribution_barriere(t_joueur player[4],int *nombrejoueur)
 {
+    ///1. Si on joue a 4j
     if (*nombrejoueur == 4)
     {
+        ///Chacun aura 5 barrieres
         for(int i = 0; i<4; i++)
         {
             player[i].barrieresR = 20/4;
         }
     }
-
+    ///2. Si on joue a 2j
     else if(*nombrejoueur == 2)
     {
+        ///Chacun aura 10 barrieres
         for(int i = 0; i<2; i++)
         {
             player[i].barrieresR = 20/2;
