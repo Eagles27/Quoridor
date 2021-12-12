44aa02625b7e1721be409abfc3bd579cb85daa9f
Pierre <70819571+swiipius@users.noreply.github.com>
2021-12-12 10:37:26 +0100

    Debur color mat vis

--- QuoridorCB/Matrice_visible.c
+++ QuoridorCB/Matrice_visible.c
@@ -30,7 +30,16 @@ void matrice_propre(int tab[17][17], t_joueur player[4], t_IA ia)//Comm OK
     printf("    "); //Coin superieur gauche
 
     ///1.1. Affichage ligne coordonnees abscisses
-    for (i = 0 ; i<17 ; i++) printf(" %c", 'A'+i);
+    for (i = 0 ; i<17 ; i++){
+        if(i%2 == 0){
+            Color(15,0);
+            printf(" %c", 'A'+i);
+        }
+        else{
+            Color(10,0);
+            printf(" %c", 'A'+i);
+        }
+    }
     printf("\n\n");
 
 
@@ -38,7 +47,14 @@ void matrice_propre(int tab[17][17], t_joueur player[4], t_IA ia)//Comm OK
     for(i=0; i<17; i++)
     {
         ///1.2. Affichage colonne coordonnees ordonnees
-        printf("%c   ",'a'+i);
+        if(i%2 == 0){
+            Color(15,0);
+            printf("%c   ",'a'+i);
+        }
+        else{
+            Color(10,0);
+            printf("%c   ",'a'+i);
+        }
 
         ///2. Affichage plateau
         for(j=0; j<17; j++)
@@ -49,9 +65,11 @@ void matrice_propre(int tab[17][17], t_joueur player[4], t_IA ia)//Comm OK
                 if(j%2==1) //Si impair alors croix simple ou double
                 {
                     if(tab[i][j]==3){
+                        Color(4,0);
                         printf("%c", 0xCE);//Mur croisement double
                     }
                     else{
+                        Color(10,0);
                         printf("%c", 0xC5);//Mur croisement simple
                     }
                 }
@@ -59,10 +77,11 @@ void matrice_propre(int tab[17][17], t_joueur player[4], t_IA ia)//Comm OK
                 else if(j%2==0) //Si pair alors barriere horizontale simple ou double
                 {
                     if(tab[i][j]==1){
-                        //printf("j%2==0");
+                        Color(10,0);
                         printf("%c%c%c", 0xC4, 0xC4, 0xC4);//Mur horizontal
                     }
                     else{
+                        Color(4,0);
                         printf("%c%c%c", 0xCD, 0xCD, 0xCD);//Mur horizontal double
                     }
                 }
@@ -72,24 +91,31 @@ void matrice_propre(int tab[17][17], t_joueur player[4], t_IA ia)//Comm OK
             {
                 if(tab[i][j]==0) //0 = case vide
                 {
+                    Color(15,0);
                     printf("   ");//Rien dans la case
                 }
                 else if(tab[i][j]==5){ //1 = Pion joueur 1
+                    Color(1,0);
                     printf(" %c ", caractereJ[0]);
                 }
                 else if(tab[i][j]==7){ //7 = Pion joueur 2
+                    Color(6,0);
                     printf(" %c ", caractereJ[1]);
                 }
                 else if(tab[i][j]==8){ //8 = Pion joueur 3
+                    Color(5,0);
                     printf(" %c ", caractereJ[2]);
                 }
                 else if(tab[i][j]==9){ //9 = Pion joueur 4
+                    Color(15,0);
                     printf(" %c ", caractereJ[3]);
                 }
                 else if(tab[i][j]==1){
+                    Color(10,0);
                     printf("%c", 0xB3);//Mur vertical
                 }
                 else if(tab[i][j]==3){ //3 = barriere double
+                    Color(4,0);
                     printf("%c", 0xBA);
                 }
                 else if(tab[i][j]==4){ //4 = case bonus
