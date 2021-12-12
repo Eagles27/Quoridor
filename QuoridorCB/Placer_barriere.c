9cd8c3d76254bdc8543c93a0d0275cc171885934
Pierre <70819571+swiipius@users.noreply.github.com>
2021-12-12 01:01:00 +0100

    commentaire placer barriere

--- QuoridorCB/Placer_barriere.c
+++ QuoridorCB/Placer_barriere.c
@@ -1,11 +1,16 @@
 #include "Header_Pierre.h"
 
+
+///Fonction renvoyant 1 si la barriere a ete posee sinon -1
+///Elle prend en entree des donnees saisies par l'utilisateur via un autre sous programme
 int placer_barriere(int tab[17][17], int place[2], char saisie)
 {
+    ///Test si la place choisie est libre
     if(!((place[0]%2==1)&&(place[1]%2==1)))
     {
         if((saisie == 'h')||(saisie == 'H'))
         {
+            //On test si la barriere sort du plateau
             if(place[1]==0)
             {
                 printf("Placement invalide\n");
@@ -14,8 +19,10 @@ int placer_barriere(int tab[17][17], int place[2], char saisie)
             }
             else
             {
+                //Sinon on regarde si la case dans la direction adjacente a celle voulue est libre
                 if((tab[place[1]][place[0]]==1)&&(tab[place[1]-1][place[0]]==1)&&(tab[place[1]-2][place[0]]==1))
                 {
+                    //On place 3 barrieres les deux devant les cases + la jonction
                     tab[place[1]][place[0]]=3;
                     tab[place[1]-1][place[0]]=3;
                     tab[place[1]-2][place[0]]=3;
@@ -23,6 +30,7 @@ int placer_barriere(int tab[17][17], int place[2], char saisie)
                 }
                 else
                 {
+                    //La case adjacente n'est pas libre donc on renvoie -1 pour que l'utilisateur place une barriere a nouveau
                     printf("Placement invalide\n");
                     system("PAUSE");
                     return -1;
@@ -32,6 +40,7 @@ int placer_barriere(int tab[17][17], int place[2], char saisie)
 
         else if((saisie == 'b')||(saisie == 'B'))
         {
+            //On test si la barriere sort du plateau
             if(place[1]==16)
             {
                 printf("Placement invalide\n");
@@ -40,8 +49,10 @@ int placer_barriere(int tab[17][17], int place[2], char saisie)
             }
             else
             {
+                //Sinon on regarde si la case dans la direction adjacente a celle voulue est libre
                 if((tab[place[1]][place[0]]==1)&&(tab[place[1]+1][place[0]]==1)&&(tab[place[1]+2][place[0]]==1))
                 {
+                    //On place 3 barrieres les deux devant les cases + la jonction
                     tab[place[1]][place[0]]=3;
                     tab[place[1]+1][place[0]]=3;
                     tab[place[1]+2][place[0]]=3;
@@ -49,6 +60,7 @@ int placer_barriere(int tab[17][17], int place[2], char saisie)
                 }
                 else
                 {
+                    //La case adjacente n'est pas libre donc on renvoie -1 pour que l'utilisateur place une barriere a nouveau
                     printf("Placement invalide\n");
                     system("PAUSE");
                     return -1;
@@ -58,6 +70,7 @@ int placer_barriere(int tab[17][17], int place[2], char saisie)
 
         else if((saisie == 'g')||(saisie == 'G'))
         {
+            //On test si la barriere sort du plateau
             if(place[0]==0)
             {
                 printf("Placement invalide\n");
@@ -66,8 +79,10 @@ int placer_barriere(int tab[17][17], int place[2], char saisie)
             }
             else
             {
+                //Sinon on regarde si la case dans la direction adjacente a celle voulue est libre
                 if((tab[place[1]][place[0]]==1)&&(tab[place[1]][place[0]-1]==1)&&(tab[place[1]][place[0]-2]==1))
                 {
+                    //On place 3 barrieres les deux devant les cases + la jonction
                     tab[place[1]][place[0]]=3;
                     tab[place[1]][place[0]-1]=3;
                     tab[place[1]][place[0]-2]=3;
@@ -75,6 +90,7 @@ int placer_barriere(int tab[17][17], int place[2], char saisie)
                 }
                 else
                 {
+                    //La case adjacente n'est pas libre donc on renvoie -1 pour que l'utilisateur place une barriere a nouveau
                     printf("Placement invalide\n");
                     system("PAUSE");
                     return -1;
@@ -84,6 +100,7 @@ int placer_barriere(int tab[17][17], int place[2], char saisie)
 
         else if((saisie == 'd')||(saisie == 'D'))
         {
+            //On test si la barriere sort du plateau
             if(place[0]==16)
             {
                 printf("Placement invalide\n");
@@ -92,8 +109,10 @@ int placer_barriere(int tab[17][17], int place[2], char saisie)
             }
             else
             {
+                //Sinon on regarde si la case dans la direction adjacente a celle voulue est libre
                 if((tab[place[1]][place[0]]==1)&&(tab[place[1]][place[0]+1]==1)&&(tab[place[1]][place[0]+2]==1))
                 {
+                    //On place 3 barrieres les deux devant les cases + la jonction
                     tab[place[1]][place[0]]=3;
                     tab[place[1]][place[0]+1]=3;
                     tab[place[1]][place[0]+2]=3;
@@ -101,6 +120,7 @@ int placer_barriere(int tab[17][17], int place[2], char saisie)
                 }
                 else
                 {
+                    //La case adjacente n'est pas libre donc on renvoie -1 pour que l'utilisateur place une barriere a nouveau
                     printf("Placement invalide\n");
                     system("PAUSE");
                     return -1;
@@ -108,6 +128,7 @@ int placer_barriere(int tab[17][17], int place[2], char saisie)
             }
         }
     }
+    ///La place choisie n'est pas libre donc on renvoie -1 pour que l'utilisateur place une barriere a nouveau
     else
     {
         printf("Placement invalide\n");
