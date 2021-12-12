90a2109c91e6429c32f084a91e3c84ca39403dc1
Pierre <70819571+swiipius@users.noreply.github.com>
2021-12-12 02:36:59 +0100

    commentaire choix_j et tour par tour

--- QuoridorCB/choix_j.c
+++ QuoridorCB/choix_j.c
@@ -1,10 +1,15 @@
 #include "Header_Pierre.h"
 
+///Determine le premier joueur
 int choix_j(int nombre)
 {
+    ///0. DDV
     int joueur = 0;
+
+    ///0.1. Initialisation clock
     srand(time(NULL));
 
+    ///1. Generation aleatoire du premier joueur
     joueur = rand()%(nombre-1+1)+1;
 
     return joueur;
